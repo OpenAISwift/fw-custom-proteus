ff277adb02ec3a56561f826cd258245c00a02106
Bryan Jimenez <58609628+OpenAISwift@users.noreply.github.com>
2025-05-18 16:03:26 +0000

    Custom Board Settings

--- board.mk
+++ board.mk
@@ -6,6 +6,5 @@ BOARDINC += $(BOARD_DIR)/generated/controllers/generated
 # defines SHORT_BOARD_NAME
 include $(BOARD_DIR)/meta-info.env
 
-# assign critical LED to a non-existent pin if you do not have it on your board
-# good old PD14 is still the default value
-# DDEFS += -DLED_CRITICAL_ERROR_BRAIN_PIN=Gpio::I15
+DDEFS += -DEFI_KLINE=TRUE
+DDEFS += -DSTM32_SERIAL_USE_USART2=TRUE
